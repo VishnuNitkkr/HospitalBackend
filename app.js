51181a1c255939ab7df57e8267d5a9cd1e158288
VishnuNitkkr <vk9509910539@gmail.com>
2024-07-23 18:27:29 +0530

    check

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ config({ path: "./config/config.env" });
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Origin, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true'); // If you need to send cookies
   next();
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 app.use(cors({
   origin: '*',
-  methods: ['GET, POST, PUT, DELETE'],
+  methods: ['GET, POST, PUT, DELETE,OPTIONS'],
   credentials: true,
   
 }));
